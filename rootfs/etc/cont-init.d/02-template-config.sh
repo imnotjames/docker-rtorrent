203d01c1dc67c61c8322657b392c40f387d130c6
James Ward <james@notjam.es>
2025-10-25 01:47:20 -0400

    feat: support `RT_EXTRA_CONFIG` to inject config values

--- rootfs/etc/cont-init.d/02-template-config.sh
+++ rootfs/etc/cont-init.d/02-template-config.sh
@@ -11,6 +11,7 @@ export RT_PREALLOCATE_TYPE=${RT_PREALLOCATE_TYPE:-0}
 export RT_PEER_PORT=${RT_PEER_PORT:-50000}
 export RT_DHT_PORT=${RT_DHT_PORT:-6881}
 export RT_SESSION_SAVE_SECONDS=${RT_SESSION_SAVE_SECONDS:-3600}
+export RT_EXTRA_CONFIG=${RT_EXTRA_CONFIG:-}
 
 template /etc/template/nginx/conf.d/rpc.conf > /etc/nginx/conf.d/rpc.conf
 template /etc/template/nginx/nginx.conf > /etc/nginx/nginx.conf
